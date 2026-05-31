ecfc6b264c6837ff6b20c94504de178c0229fd68
awesomesuzjon <bohorasujan39@gmail.com>
2026-05-31 13:29:23 +1000

    feat -> repo setup and init

---
+++ src/data/dashboardData.ts
@@ -0,0 +1,35 @@
+export const upcomingMatches = [
+    {
+        id: 1,
+        home: "Argentina",
+        away: "Brazil",
+        time: "15 Jun - 8:00 PM",
+    },
+    {
+        id: 2,
+        home: "France",
+        away: "Germany",
+        time: "16 Jun - 11:00 PM",
+    },
+    {
+        id: 3,
+        home: "Spain",
+        away: "England",
+        time: "17 Jun - 8:00 PM",
+    },
+];
+
+export const leaderboard = [
+    {
+        name: "Alex",
+        points: 15820,
+    },
+    {
+        name: "Emma",
+        points: 14230,
+    },
+    {
+        name: "Sujan",
+        points: 12890,
+    },
+];
